46f1d5bcf461def5a01bab0eb60afc0f653865c2
Maryjo-obi <ify19999@gmail.com>
2022-09-08 09:43:42 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return - Always 0 (success)
+ *\
+
+ int main(void)
+
+ {puts ("Programming is like building a multilinual puzzle" \n");
+ return (0);
+ }
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -2,3 +2,4 @@ preprocessor
 compiles a C file but does not link
 script that generates the assembly code of a C code and save it in an output file
 script that compiles a C file and an executable
+using the function puts
