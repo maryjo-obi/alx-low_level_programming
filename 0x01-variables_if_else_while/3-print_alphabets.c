5524991c1b6d9e2fe31d023a2b030b8259f0a780
maryjo <ify19999@gmail.com>
2022-09-09 22:15:31 +0100

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,19 +9,20 @@
 
 int main(void)
 {
-   char ch;
-   for (ch = 'a' ; ch <= 'z' ; ch++)
-     {
-	putchar(ch);
-     }
-   for (ch = 'A' ; ch <= 'Z' ; ch++)
-     {
-	putchar(ch);
-     } 
+	char ch;
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
+	{
+		putchar(ch);
+	} 
 
-   putchar('\n');
+	putchar('\n');
 
-   return (0);
+	return (0);
 }
+
 
 
