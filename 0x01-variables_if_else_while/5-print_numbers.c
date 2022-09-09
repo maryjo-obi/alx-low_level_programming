b90d0cb128246af83a2ea5ca4536cf2a07b0b26c
maryjo <ify19999@gmail.com>
2022-09-09 19:36:30 +0100

    if_else_while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio>
+
+/**
+ * main - print all single digit of base 10,starting from 0
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printf("\n");
+
+	return (0);
+}
+
+
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio>
+
+/**
+ * main - print all single digit of base 10 using putchar function only
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
+}
