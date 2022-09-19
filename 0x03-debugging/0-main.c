774b013b5d8191e78c0aa9c295cd36eecf3c374c
maryjo <ify19999@gmail.com>
2022-09-19 15:05:31 +0100

    debug

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -5,12 +5,14 @@
 * Return: 0
 */
 
-int main(void)
+void positive_or_negative(int i);
 {
         int i;
 
+	{
         i = 98;
-        positive_or_negative(i);
+        int main(void);
+	}
 
         return (0);
 }
