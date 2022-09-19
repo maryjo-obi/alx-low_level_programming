a1b64fabcea9258dd9232570b7c1f1578919840f
maryjo <ify19999@gmail.com>
2022-09-19 20:58:32 +0100

    array

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string.
+ *
+ * @str: The string to get the length of.
+ *
+ * Return: The length of @str.
+ */
+
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string.
+ * @str: The string to get the length of.
+ *
+ * Return: The length of @str.
+ */
+
+size_t _strlen(const char *str)
+{
+	sizeof length = 0;
+
+	while (*str++)
+		length++;
+
+	return (length);
+}
+
