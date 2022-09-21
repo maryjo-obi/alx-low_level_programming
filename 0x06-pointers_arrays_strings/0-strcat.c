a7ffe3efb8ae40a8c1aab19811a64f0f60984893
maryjo <ify19999@gmail.com>
2022-09-21 18:59:14 +0100

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -15,10 +15,13 @@
 
 char *_strcat(char *dest, char *src)
 {
-	while (des[index++])
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
 		dest_len++;
-	for (index = 0; src [index]; index++)
-		dest [dest_len++] = scr [index];
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
 
 	return (dest);
 }
